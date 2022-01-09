fa3fab0619569d513f6f11e62ee703d79ec8bcce
Kirill12a <kirilldrozd12@gmail.com>
2022-01-09 23:56:14 +0300

    first screen ✅

--- rickAndMortyPersons/Controllers/HomeCollectionViewController.swift
+++ rickAndMortyPersons/Controllers/HomeCollectionViewController.swift
@@ -10,67 +10,76 @@ import UIKit
 
 class HomeCollectionViewController: UICollectionViewController{
     
-    
-    var homeViewModel = HomeViewModel()
+    private var homeViewModel = HomeViewModel()
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         setupCollectionView()
         setupDataSource()
         homeViewModel.fetchCharacters()
         setupNavigation()
     }
     
-    func setupCollectionView(){
+    func setupCollectionView()
+    {
         self.collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: HomeCollectionViewCell.reuseIdentifier)
         collectionView.backgroundColor = .systemBackground
     }
     
-    func setupNavigation(){
+    func setupNavigation()
+    {
         self.navigationItem.title = "Персонажы"
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
     
-    func setupDataSource() {
+    func setupDataSource()
+    {
         homeViewModel.dataSource = UICollectionViewDiffableDataSource<Sections, Results>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, results) -> UICollectionViewCell? in
-            
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCollectionViewCell.reuseIdentifier, for: indexPath) as? HomeCollectionViewCell
             cell?.populate(character: results)
-            cell?.backgroundColor = .orange
             return cell
         })
     }
 }
 
 
-extension HomeCollectionViewController: UICollectionViewDelegateFlowLayout {
+//MARK: Delegate
+extension HomeCollectionViewController: UICollectionViewDelegateFlowLayout
+{
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
         let width: CGFloat = view.bounds.width / 2 - 16
         let height: CGFloat = 300
         
         return CGSize(width: width, height: height)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 15
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat
+    {
+         25
     }
+    
+    // не робит -------- //
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat
+    {
+        return 1
+    } // ------------ //
 
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 5
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-//        return UIEdgeInsets(top: 2, left: 0, bottom: 8, right: 8) так норм
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets
+    {
+        return UIEdgeInsets(top: 0, left: 3, bottom: 0, right: 3)
     }
 
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        homeViewModel.didSelectItemAt(indexPath) { controller in
+        homeViewModel.didSelectItemAt(indexPath)
+        { controller in
             navigationController?.pushViewController(controller, animated: true)
         }
     }
 
-    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath)
+    {
         homeViewModel.willDisplay(indexPath)
     }
 }
